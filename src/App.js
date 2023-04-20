b5d3c038e6fcc224bffd9870bc2d91f2bac2633c
nicholas1990 <nicholas.angelucci@gmail.com>
2023-04-20 19:37:32 +0200

    Minor Fix

--- src/App.js
+++ src/App.js
@@ -2,69 +2,49 @@ import logo from './img/topmenulogo.png';
 import topimage from './img/Group15.png';
 import './App.css';
 
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TopGrid />
-        
-        
-        <AboutPage />
-        <BuyButton />
-        
+        <div class="container h-100">
+
+          <TopNavbar />
+
+          <div class="row p-5">
+            <TopImage />
+          </div>
+          
+          <PageContent />
+          <BuyButton />
+
+        </div>
       </header>
     </div>
   );
 }
-function MyButton() {
-  return (
-    <button type="button" className='bordered-button'>Click Me!</button> 
-  );
-}
-function BuyButton() {
-  return (
-    <button type="button" className='buy-button'>Buy LIRA!</button> 
-  );
-}
 
-function TopGrid () {
+function TopNavbar() {
   return (
-    <div class="container">
-  <div class="row p-4">
-    <div class="col-2">
-      <Logo />
+    <div class="row p-4">
+      <div class="col-2">
+        <Logo />
+      </div>
+      <div class="col-8">
+        <TopMenu />
+      </div>
+      <div class="col-2">
+        <MyButton />
+      </div>
     </div>
-    <div class="col-8">
-      <TopMenu />
-    </div>
-    <div class="col-2">
-      <MyButton />
-    </div>
-  </div>
-  <div class="row p-5">
-    <div class="col">
-      <TopImage />
-    </div>
-  </div>
-</div>
   );
 }
 
-function AboutPage() {
+function Logo() {
   return (
-    <div id='about-page'>
-      <h1 className="title">{project.name}</h1>
-      <p>The revolution of Cryptocurrency <br /> Join the Satoshi LIRA Ecosystem</p>
-
-    </div>
-
-
+    <a href={`${window.location.protocol}//${window.location.hostname}${window.location.port !== 80 && ":3000"}`}>
+      <img src={logo} class="img-fluid" alt="Homepage Satoshi LIRA Project" />
+    </a>
   )
-}
-const project = {
-  name: 'Satoshi LIRA'
 }
 
 function TopMenu() {
@@ -78,24 +58,38 @@ function TopMenu() {
         <li className='menuline'>Faq</li>
       </ul>
     </>
-
   )
 }
 
-function Logo() {
+function MyButton() {
   return (
-    <a href={`${window.location.protocol}//${window.location.hostname}${window.location.port !== 80 && ":3000"}`}>
-      <img src={logo} class="img-fluid" alt="Homepage Satoshi LIRA Project" />
-    </a>
-  )
+    <button type="button" className='bordered-button'>Click Me!</button> 
+  );
 }
 
 function TopImage() {
   return(
-    <img src={topimage} class="img-fluid" alt="Satoshi LIRA Cryptocurrecy Revolution" />
+    <img src={topimage} className="img-fluid" alt="Satoshi LIRA Cryptocurrecy Revolution" />
   )
 }
 
+function PageContent() {
+  return (
+    <div id='about-page'>
+      <h1 className='title'>{ project.name }</h1>
+      <p>The revolution of Cryptocurrency <br /> Join the Satoshi LIRA Ecosystem</p>
+    </div>
+  )
+}
 
+const project = { 
+  name: 'Satoshi LIRA'
+}
+
+function BuyButton() {
+  return (
+    <button type="button" className='buy-button'>Buy LIRA!</button> 
+  );
+}
 
 export default App;
