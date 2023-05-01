fd4c02021569d2a210fba72722ebd2241818049c
n0x <n0x@satoshilira.com>
2023-05-01 15:16:04 +0200

    update, take price and swap

--- src/Router.js
+++ src/Router.js
@@ -5,10 +5,14 @@ import Swap from "./components/Swap";
 import Tokens from "./components/Tokens";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "antd";
+import { useConnect, useAccount } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
 const { Footer, Content } = Layout;
 
 function Router() {
+  const { address, isConnected } = useAccount()
+
   return (
     <Layout>
       <Header/>
