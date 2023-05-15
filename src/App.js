a7d7b846fe7b79dace71be1e1f25bd3bbc505ea3
n0x <n0x@satoshilira.com>
2023-05-15 12:02:41 +0200

    add countdown presale

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Flex from "./components/Flex";
 import topImage from "./img/slogan.png";
+import Countdown from "./components/Countdown";
+
 
 function App() {
   return (
@@ -31,6 +33,12 @@ function App() {
               political and economical freedom of its community.
             </h3>
           </div>
+        </Flex>
+          <div>
+            <Countdown></Countdown>
+          </div>
+        <Flex>
+          
         </Flex>
         {/* <Flex flex={1} justifyContent="center" alignItems="center">
         <Flex flexDirection="column" width="30%">
